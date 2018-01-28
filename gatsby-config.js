b00f9de41967ac35587be0de2894dd0e6c1abce4
Ryan Patterson <ryan@Ryans-MacBook-Air.local>
2018-01-28 10:14:12 -0800

    i mean md renders, but we gotta simplify it

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     title: 'Web Poetry Book',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-catch-links',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages/writing`,
+        name: "pages",
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
 };
